368e2ff2c35e565fe1108a8710bd09917f807b4c
Adela <adela.yohana@gmail.com>
2020-01-14 11:15:54 -0800

    Add a second card and margin to cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+  $(".click-walrus").click(function() {
     $("#hide-walrus").fadeToggle();
     $("#show-walrus").fadeToggle();
+  });
+  $(".click-ice").click(function() {
+    $("#hide-ice").fadeToggle();
+    $("#show-ice").fadeToggle();
   });
 });
